b3fb477624b9c31ab69028cd46ed003c31ba07c0
Terri Prifti <teri.1998@hotmail.com>
2017-10-19 21:11:19 +0200

    Excercise 2

---
+++ beginner/printf/main1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+}
+
+// The '\n' in the end of text is called escape sequence that denotes a newline character.
---
+++ beginner/printf/main2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("If at first you don\'t succeed, try, try, try again!\n");
+    return 0;
+}
+
+// The \' sequence in the middle of the text to be displayed is called an escape sequence. Here it’s
+// a special way of including a single quote in the text because single quotes are usually used to indicate
+// where a character constant begins and ends. You can try recompiling the program, relinking it,
+// and running it again once you’ve altered the source. With a following wind and a bit of luck you have
+// now edited your first program. You’ve written a program using the editor, edited it, and compiled,
+// linked, and executed it.
